5953d03c866264aff85ad2cf0346f2b4715c19b0
Lukasz Wolnik <lukasz@wolnik.co.uk>
2019-10-23 12:19:43 +0100

    Build version added

--- index.js
+++ index.js
@@ -2,16 +2,19 @@ const config = require('dotenv/config');
 const express = require('express');
 var cors = require('cors')
 
+const BUILD_VERSION = 2;
+const buildVersion = `Build #${BUILD_VERSION}`;
+
 const app = express();
 
 app.use(cors())
 
 app.get('/', (req, res) => {
-    return res.send('Amazon Login API - Build 1');
+    return res.send(`Amazon Login API - ${buildVersion}`);
 });
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () =>
-  console.log(`Amazon Login API is listening on port ${port}...`),
+  console.log(`Amazon Login API ${buildVersion} is listening on port ${port}...`),
 );
